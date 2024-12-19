1decfb12b2190bb77d47130edf33fe51dcf105ed
Hugo <hg.piat@gmail.com>
2024-12-19 11:54:46 +0100

    BrickBehaviour

---
+++ src/Component/Behaviour/BrickBehaviour.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "../AComponent.h"
+
+class BrickBehaviour : public AComponent
+{
+
+public:
+	BrickBehaviour();
+	virtual ~BrickBehaviour() = default;
+
+	void Init();
+
+	virtual void Update() override;
+
+};
